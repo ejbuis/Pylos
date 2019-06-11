ce92e6ed2680a8fd8f2f1ab26b3c855f512eca15
ejbuis <ejbuis@PC-16650.tsn.tno.nl>
2019-06-11 14:39:02 +0200

    python version3 compatible

--- src/spectrogram.py
+++ src/spectrogram.py
@@ -13,11 +13,15 @@ import string
 
 from bandpass_filter import *
 
+def usage():
+    print("Usage:  %s  <wav-file> <t_min (s)> <t_max (s)> \n"
+          % os.path.basename(sys.argv[0]))
+
+
 def get_timestamp(filename, begin_time):
     s = os.path.splitext(os.path.basename(filename))[0][10:]
     extra_minute = int(begin_time/60.)
     extra_second = int(begin_time - extra_minute*60.)
-
     hours = int(s[6:8])
     minutes = int(s[8:10]) + extra_minute
     seconds = int(s[10:12]) + extra_second
@@ -39,16 +43,18 @@ def graph_spectrogram(wav_file, begin, end, cutoff_low, cutoff_high, order):
     pylab.subplot(111)
     pylab.title('spectrogram of %r' % wav_file)
     nfft = 1024
-    data = sound_info[begin*Fs: end*Fs]
+    data = sound_info[int(begin*Fs): int(end*Fs)]
     data = butter_bandpass_filter(data, cutoff_low, cutoff_high, Fs, order)
     pylab.specgram(data, Fs=Fs, NFFT = nfft, \
-                   noverlap =int(nfft/2), cmap='viridis_r', \
+                   noverlap =int(nfft/2),
+                   cmap='nipy_spectral', \
                    mode = 'psd')
-
     pylab.colorbar()
-    datestring = get_timestamp(wav_file, begin)
-    pylab.xlabel("Time (seconds since {}) [s]".format(datestring), \
-                 size = 16, ha ='right', x=1.0)
+    
+    #    datestring = get_timestamp(wav_file, begin)
+    #    pylab.xlabel("Time (seconds since {}) [s]".format(datestring), \
+        #                 size = 16, ha ='right', x=1.0)
+    pylab.xlabel("Time (seconds)", size = 16, ha ='right', x=1.0)
     pylab.ylabel("PSD", size = 16, ha ='right', position=(0,1))
     pylab.savefig('spectrogram.png')
     pylab.show()
@@ -66,15 +72,19 @@ import string
 def main(argv):
     begin = 0
     end = 100
+    
+    if len(argv) < 2:
+        usage()
+        sys.exit()
     if len(argv) > 1:
         filename  = argv[1]
     if len(argv) > 2:
-        begin = string.atoi(argv[2]) # in seconds
-        end = string.atoi(argv[3]) # seconds
+        begin = float(argv[2]) # in seconds
+        end = float(argv[3]) # seconds
 
     filename = argv[1]
-    cutoff_low = 5000
-    cutoff_high = 70000
+    cutoff_low = 100.
+    cutoff_high = 30000.
     order = 5 
     graph_spectrogram(filename, begin, end, cutoff_low, cutoff_high, order)
 
